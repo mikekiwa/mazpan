f608e8c84faadd7f2acf39e73d08f8f423cd137e
mespina1986 <mespina1986@9f2f3bea-e32e-11de-b4b3-6d349e4f6f86>
2010-08-13 02:59:47 +0000

    VErsion no testeada, actualizacion por formateo toshi

---
+++ Server/App_Code/Merma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+/// <summary>
+/// Summary description for Merma
+/// </summary>
+public class Merma
+{
+    private DataTable dt1;//
+    private DataTable dt2;//
+	public Merma()
+	{
+	}
+    public DataTable Mermas
+    {
+        set { dt1 = value; }
+        get { return dt1; }
+    }
+    public DataTable Producido
+    {
+        set { dt2 = value; }
+        get { return dt2; }
+    }
+}
